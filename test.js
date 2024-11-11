9a783396283a9db4230906b27ee8f93d63c113d9
Kuanysh <tkuanys767@gmail.com>
2024-11-11 11:34:20 +0600

    submit exam4

---
+++ test.js
@@ -0,0 +1,58 @@
+// task
+
+// let month = Number(prompt("1-ден  12-ге дейін санды енгіземіз"))
+// let num = 0
+
+// switch(month){
+//     case(12 || 1 || 2):
+//         alert("АЙ"+month+"МАУСЫМ: Winter")
+//         break
+//     case (3 || 4 || 5):
+//         alert("АЙ"+month+"МАУСЫМ: Spring")
+//         break
+//     case (6 || 7 | |8):
+//         alert("АЙ"+month+"МАУСЫМ: Summer")
+//         break
+//     case (9 || 10 || 11):
+//         alert("АЙ"+month+"МАУСЫМ: Autumn")
+//         break
+//     default:
+//         alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+// }
+
+// task 2
+
+// let a = +prompt("a саны"); 
+// let b = +prompt("b саны")
+// if(a>b){
+//     alert("a саны b санынан улкен")
+// }
+// if(a<b){
+//     alert("b саны a санынан улкен")
+// }
+// else{
+//     alert("a және b тен болады")
+// }
+// alert("a және b орташа мәні  "+(a+b)/2)
+
+
+// task 3
+
+let a = +prompt("1 сан")
+let b = +prompt("2 сан")
+let c = +prompt("3 сан")
+if(a>=b && a>=c){
+    alerty("Ең үлкен және ең кішкентай санның айырмасы: " + (a-b) || (a-c))
+}
+if(b>=a && b>=c){
+    alert("Ең үлкен және ең кішкентай санның айырмасы: " + (b-a) || (b-c))
+}
+if(c>=a && c>=b){
+    alert("Ең үлкен және ең кішкентай санның айырмасы: " + (c-a) || (c-a))
+}
+else{
+    alert("0 тен болады")
+}
+
+
+
