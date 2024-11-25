66a9371f6ed8705132ad90f226f0343c97fd53b6
Kuanysh <tkuanys767@gmail.com>
2024-11-25 11:56:17 +0600

    Аралық бақылау емтиханы #5

--- test.js
+++ test.js
@@ -10,7 +10,7 @@
 //     case (3 || 4 || 5):
 //         alert("АЙ"+month+"МАУСЫМ: Spring")
 //         break
-//     case (6 || 7 | |8):
+//     case (6 || 7 || 8):
 //         alert("АЙ"+month+"МАУСЫМ: Summer")
 //         break
 //     case (9 || 10 || 11):
@@ -38,21 +38,64 @@
 
 // task 3
 
-let a = +prompt("1 сан")
-let b = +prompt("2 сан")
-let c = +prompt("3 сан")
-if(a>=b && a>=c){
-    alerty("Ең үлкен және ең кішкентай санның айырмасы: " + (a-b) || (a-c))
-}
-if(b>=a && b>=c){
-    alert("Ең үлкен және ең кішкентай санның айырмасы: " + (b-a) || (b-c))
-}
-if(c>=a && c>=b){
-    alert("Ең үлкен және ең кішкентай санның айырмасы: " + (c-a) || (c-a))
-}
-else{
-    alert("0 тен болады")
-}
+// let a = +prompt("1 сан")
+// let b = +prompt("2 сан")
+// let c = +prompt("3 сан")
+// if(a>=b && a>=c){
+//     alerty("Ең үлкен және ең кішкентай санның айырмасы: " + (a-b) || (a-c))
+// }
+// if(b>=a && b>=c){
+//     alert("Ең үлкен және ең кішкентай санның айырмасы: " + (b-a) || (b-c))
+// }
+// if(c>=a && c>=b){
+//     alert("Ең үлкен және ең кішкентай санның айырмасы: " + (c-a) || (c-a))
+// }
+// else{
+//     alert("0 тен болады")
+// }
 
 
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// tasc 1
+
+// let number1 = +prompt("1 san")
+// let number2 = +prompt("2 san")
+// function calkulatorNume1(number1, number2){
+//     console.log("qocyndisi  " + (number1+number2));
+//     number1+number2
+//     console.log("azaityndisi  " + (number1-number2));
+//     number1-number2
+//     console.log("kobeytindisi  " + (number1*number2));
+//     number1*number2
+//     console.log("bolindisi  " + (number1/number2));
+//     number1/number2
+// }
+// calkulatorNume1(number1, number2);
+
+// tasc 2
+let sum = 0 
+let tagam
+let maz = 0
+
+function superMarket(sum, tagam, maz){
+    let i = 1;
+    while(true){
+        let shygunAty = +prompt(i + "shygyn aty")
+        if(shygunAty.toLowerCase() == "ayktau"){
+            console.log(`syfyn nomer #$[i]: ayktau`)
+            console.log(`ketken summa $[sum]`)
+            console.log()
+        }
+        else{
+            let shygunBafa = +prompt(i + "shygyn bafa")
+            console.log(`syfyn nomer #$[i]:$[shygunAty] $[shygunBafa]`);
+            sum = sum + shygunBafa;
+        }
+    }
+    i++
+}
+superMarket()
+
+// tasc 3
 
