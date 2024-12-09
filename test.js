f27096268586bec540fa26485ec90a655501bb60
Kuanysh <tkuanys767@gmail.com>
2024-12-09 11:50:51 +0600

    Аралық бақылау емтиханы #6

--- test.js
+++ test.js
@@ -1,101 +1,61 @@
-// task
+//1 test///////////////////////////////////////////////////
 
-// let month = Number(prompt("1-ден  12-ге дейін санды енгіземіз"))
-// let num = 0
+eshop = {
+    name1: "Sulpak",
+    phoneNumber: 87074531808,
+    products:[
+        {
+            productName:"iphone PRO",
+            category:"Электрогика",
+            price:700000,
+            stock:30,   
+        },
+        {
+            productName:"Samsung",
+            category:"Электрогика",
+            price:500000,
+            stock:20,   
+        },
+        {
+            productName:"Meizu",
+            category:"Электрогика",
+            price:300000,
+            stock:60,   
+        },
+        {
+            productName:"lenova ",
+            category:"Электрогика",
+            price:350000,
+            stock:40,   
+        },
+        {
+            productName:"Samsung A20",
+            category:"Электрогика",
+            price:200000,
+            stock:90,   
+        },
+    ],
+}
+console.log(eshop);
 
-// switch(month){
-//     case(12 || 1 || 2):
-//         alert("АЙ"+month+"МАУСЫМ: Winter")
-//         break
-//     case (3 || 4 || 5):
-//         alert("АЙ"+month+"МАУСЫМ: Spring")
-//         break
-//     case (6 || 7 || 8):
-//         alert("АЙ"+month+"МАУСЫМ: Summer")
-//         break
-//     case (9 || 10 || 11):
-//         alert("АЙ"+month+"МАУСЫМ: Autumn")
-//         break
-//     default:
-//         alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
-// }
+///2 test ///////////////////////////////////////////////////////////////////////////////////
+let newNameEshop = prompt("дүкен атауы")
+changeShopName(newNameEshop)
+function changeShopName(NameEshopName){
+    eshop.name1=NameEshopName
+}
+console.log(eshop);
 
-// task 2
-
-// let a = +prompt("a саны"); 
-// let b = +prompt("b саны")
-// if(a>b){
-//     alert("a саны b санынан улкен")
-// }
-// if(a<b){
-//     alert("b саны a санынан улкен")
-// }
-// else{
-//     alert("a және b тен болады")
-// }
-// alert("a және b орташа мәні  "+(a+b)/2)
-
-
-// task 3
-
-// let a = +prompt("1 сан")
-// let b = +prompt("2 сан")
-// let c = +prompt("3 сан")
-// if(a>=b && a>=c){
-//     alerty("Ең үлкен және ең кішкентай санның айырмасы: " + (a-b) || (a-c))
-// }
-// if(b>=a && b>=c){
-//     alert("Ең үлкен және ең кішкентай санның айырмасы: " + (b-a) || (b-c))
-// }
-// if(c>=a && c>=b){
-//     alert("Ең үлкен және ең кішкентай санның айырмасы: " + (c-a) || (c-a))
-// }
-// else{
-//     alert("0 тен болады")
-// }
+/// 3 test /////////////////////////////////////////////////////////////////
+let newPhoneEshop = +prompt(" нөмір ")
+changePhoneNumber(newPhoneEshop)
+function changePhoneNumber(phoneNumberPhone){
+    eshop.phoneNumber=phoneNumberPhone
+}
+console.log(eshop);
 
 
 
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// tasc 1
 
-// let number1 = +prompt("1 san")
-// let number2 = +prompt("2 san")
-// function calkulatorNume1(number1, number2){
-//     console.log("qocyndisi  " + (number1+number2));
-//     number1+number2
-//     console.log("azaityndisi  " + (number1-number2));
-//     number1-number2
-//     console.log("kobeytindisi  " + (number1*number2));
-//     number1*number2
-//     console.log("bolindisi  " + (number1/number2));
-//     number1/number2
-// }
-// calkulatorNume1(number1, number2);
 
-// tasc 2
-// let sum = 0 
-// let tagam
-// let maz = 0
-
-// function superMarket(sum, tagam, maz){
-//     let i = 1;
-//     while(true){
-//         let shygunAty = +prompt(i + "shygyn aty")
-//         if(shygunAty.toLowerCase() == "ayktau"){
-//             console.log(`syfyn nomer #$[i]: ayktau`)
-//             console.log(`ketken summa $[sum]`)
-//             console.log()
-//         }
-//         else{
-//             let shygunBafa = +prompt(i + "shygyn bafa")
-//             console.log(`syfyn nomer #$[i]:$[shygunAty] $[shygunBafa]`);
-//             sum = sum + shygunBafa;
-//         }
-//     }
-//     i++
-// }
-// superMarket()
-
-// tasc 3
 
